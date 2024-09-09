52c2617cce21abe1d6d56ad6518676c813291cdd
Sieu To <121605093+sieutotu@users.noreply.github.com>
2024-09-09 16:11:49 +0700

    Update Modifiers.swift

--- Sources/PopupView/Modifiers.swift
+++ Sources/PopupView/Modifiers.swift
@@ -11,7 +11,7 @@ extension View {
 
     public func popup<PopupContent: View>(
         isPresented: Binding<Bool>,
-        uniqueId: (() -> String)?,
+        uniqueId: (() -> String)? = nil,
         @ViewBuilder view: @escaping () -> PopupContent,
         customize: @escaping (Popup<PopupContent>.PopupParameters) -> Popup<PopupContent>.PopupParameters
         ) -> some View {
